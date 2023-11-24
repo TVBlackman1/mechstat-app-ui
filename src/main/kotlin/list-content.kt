6df18330878c4e420a888685aba395f44f15b3ca
tvblackman1 <vbif.misha2012@yandex.ru>
2023-11-24 23:37:01 +0300

    reworked list buttons

--- src/main/kotlin/list-content.kt
+++ src/main/kotlin/list-content.kt
@@ -1,11 +1,24 @@
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ListContent() {
+    val density = LocalDensity.current
+    var columnWitdh by remember {
+        mutableStateOf(0.dp)
+    }
     var page by remember { mutableStateOf(1) }
     var experiments by remember {
         mutableStateOf<ArrayList<ListResponse>>(ArrayList())
@@ -13,21 +26,30 @@ fun ListContent() {
     LaunchedEffect(page) {
         experiments = getData(page)
     }
-    Column {
+    Column(
+        modifier = Modifier.onSizeChanged {size ->
+            columnWitdh = density.run {size.width.toDp() }
+        },
+    ) {
         ListComponent(experiments)
-        Row {
-            Button(onClick = {
-                if (page > 1) {
-                    page--
-                }
-            }) {
-                Text("prev")
-            }
-            Button(onClick = {
-                page++
-            }) {
-                Text("next")
-            }
+        Row(
+            modifier = Modifier.width(columnWitdh),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            IconButton(
+                content = {Icon(Icons.Default.KeyboardArrowLeft, "left")},
+                onClick = {
+                    if (page > 1) {
+                        page--
+                    }
+                },
+            )
+            IconButton(
+                content = {Icon(Icons.Default.KeyboardArrowRight, "right")},
+                onClick = {
+                    page++
+                },
+            )
         }
     }
 }
